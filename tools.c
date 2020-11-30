84cae200b93c085ad91f660261a600dc172fa3da
zkubli <zkubli@student.42.fr>
2020-11-30 11:44:25 -0500

    test

--- tools.c
+++ tools.c
@@ -6,7 +6,7 @@
 /*   By: zacharykubli <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 20:16:49 by zacharyku         #+#    #+#             */
-/*   Updated: 2020/11/30 11:43:05 by zacharyku        ###   ########.fr       */
+/*   Updated: 2020/11/30 11:44:21 by zacharyku        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void    ray_init(t_data *d, double ratio)
 	d->ray.sideDist.x *= d->ray.deltaDist.x;
 }
 
-int		check_map(t_data *d, t_point *p)
+int		get_map_point(t_data *d, t_point *p)
 {
 	if (p->x > d->map.rowlen || p->x < 0 ||
 			p->y * d->map.rowlen > d->map.tablelen || p->y < 0)
@@ -69,7 +69,7 @@ double	ray_cast(t_data *d)
 			d->ray.hit.y += d->ray.inc.y;
 			d->ray.is_NS = 1;
 		}
-		if (check_map(d, &d->ray.hit) == 1)
+		if (get_map_point(d, &d->ray.hit) == 1)
 			hit_found = 1;
 	}
 	if (d->ray.is_NS)
