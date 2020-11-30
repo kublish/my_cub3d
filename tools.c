771e571cae75eb0c47d9b01e6c4989fde642c175
zkubli <zkubli@student.42.fr>
2020-11-30 11:50:27 -0500

    test

--- tools.c
+++ tools.c
@@ -6,7 +6,7 @@
 /*   By: zacharykubli <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 20:16:49 by zacharyku         #+#    #+#             */
-/*   Updated: 2020/11/30 11:44:21 by zacharyku        ###   ########.fr       */
+/*   Updated: 2020/11/30 11:50:16 by zacharyku        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int		get_map_point(t_data *d, t_point *p)
 	if (p->x > d->map.rowlen || p->x < 0 ||
 			p->y * d->map.rowlen > d->map.tablelen || p->y < 0)
 		return (-1);
-	return (d->map.table[p->x + p->y *d->map.rowlen]);
+	return (d->map.table[p->x + p->y d->map.rowlen]);
 }
 
 double	ray_cast(t_data *d)
