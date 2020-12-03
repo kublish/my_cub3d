e2080fc48537e2057b8842bc1cc21fa4e513fe4d
zkubli <zkubli@student.42.fr>
2020-12-03 16:48:14 -0500

    test

--- render.c
+++ render.c
@@ -6,7 +6,7 @@
 /*   By: zacharykubli <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/07 11:58:56 by zacharyku         #+#    #+#             */
-/*   Updated: 2020/12/03 16:45:24 by zacharyku        ###   ########.fr       */
+/*   Updated: 2020/12/03 16:48:12 by zacharyku        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void render_col(t_data *data, int x, double perp_dist)
 				color = 0x00333333; //floor color
 			else
 				color = (data->ray.is_NS ? 0x00999999 : 0x00CCCCCC); //wallcolor
-			data->screen[x + y++ * data->screen.sl] = color;
+			data->screen.adr[x + y++ * data->screen.sl] = color;
 	}  
 }
 
@@ -51,6 +51,6 @@ void render_screen(t_data *d)
 		render_col(d, i, perp_dist);
 		i++;
     }
-	mlx_put_image_to_window(d->mlx_ptr, d->win, 0, 0);
+	mlx_put_image_to_window(d->mlx_ptr, d->win, d->screen.ptr, 0, 0);
 	fprintf(stderr, "render end.\n");
 }
