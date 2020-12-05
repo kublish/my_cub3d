45b5ea2527432fc0b74694c1d2dfc77366248874
zkubli <zkubli@student.42.fr>
2020-12-04 19:27:48 -0500

    test

--- render.c
+++ render.c
@@ -6,7 +6,7 @@
 /*   By: zacharykubli <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/07 11:58:56 by zacharyku         #+#    #+#             */
-/*   Updated: 2020/12/04 19:25:27 by zacharyku        ###   ########.fr       */
+/*   Updated: 2020/12/04 19:27:38 by zacharyku        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ void	render_col(t_data *data, int x, double perp_dist)
 			{
 				c.tcord.y = ((int)c.tex.height *
 						(y - c.wall_start) / ((double)(c.wall_size)));
-				color = c.tex.adr[c.tcord.x + c.tcord.y * c.tex_width];
+				color = c.tex.adr[c.tcord.x + c.tcord.y * c.tex.width];
 			}
 			//fprintf(stderr, "writing to screen x: %4d, y:%4d\n", x, y);
 			((int *)(data->screen.adr))[x + y++ * data->res.x] = color;
